5de40b4cda2d60da792bd896fc33cfc4918c2ad4
Jaissica <jaissica.hora@gmail.com>
2023-11-14 16:14:15 -0500

    Gallery update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile.js'
+import Gallery from './components/gallery.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Gallery />
     </div>
   )
 }
